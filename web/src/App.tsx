20bcc568c1580cf7622116c11e1e73fcde984c7c
jackdenio <deniogomes@gmail.com>
2023-09-27 01:30:40 -0300

    Aula 01 - Primeiros passos: UI da aplicação

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,69 @@
 import { Button } from "./components/ui/button";
-import { Github } from "lucide-react"
+import { Separator } from "./components/ui/separator";
+import { Github, FileVideo } from "lucide-react"
+import { Textarea } from "./components/ui/textarea";
+import { Label } from "./components/ui/label";
 
 export function App() {  
 
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <div className="px-6 py-3 flex items-center justify-between border-b">
         <h1 className="text-xl font-bold">nlw-ia</h1>
 
         <div className="flex items-center gap-3">
           <span className="text-sm text-muted-foreground">
-            Desenvolvido com ❤️ no NLW Rocketseat
+            Desenvolvido com :mate: no NLW Rocketseat
           </span>
+
+          <Separator orientation='vertical' className="h-6" />
+
           <Button variant="outline">
-            <Github/>
+            <Github className="w-4 h-4 mr-2"/>
             Github
           </Button>
         </div>
       </div>
+
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea
+              placeholder="Inclua o prompt para a IA"
+              className="resize-none p-4 leading-relaxed" 
+            />
+            <Textarea
+              placeholder="Resultado gerado pela IA" 
+              readOnly 
+              className="resize-none p-4 leading-relaxed"
+            />
+
+          </div>
+
+          <p className="text-sm text-muted-foreground">
+            Lembre-se: você pode utilizar a variável <code className="text-violet-400">{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado
+          </p>
+        </div>
+        <aside className="w-80 space-y-6">
+          <form className="space-y-6">
+            <label 
+              htmlFor="video"
+              className="border flex rounded-md aspect-video cursos-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-yellow-500/50"
+            >
+              <FileVideo className="w-4 h-4" />
+              Selcionar video
+            </label>
+            <input type="file" id="video" accept="video/mp4" className="sr-only" />
+            <Separator />
+            <div className="space-y-1">
+              <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
+              <Textarea id="transcription_prompt" className="min-h-20 leading-relaxed"></Textarea>
+            </div>
+          </form>
+
+        </aside>
+
+      </main>
     </div>
   )
 }
