862b98c4066d284173eb0c20beedd7f1bd906bc0
jackdenio <deniogomes@gmail.com>
2023-09-30 15:36:39 -0300

    Aula 01 - Primeiros passos: UI da aplicação

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,11 @@
 import { Button } from "./components/ui/button";
 import { Separator } from "./components/ui/separator";
-import { Github, FileVideo } from "lucide-react"
+import { Github, FileVideo, Upload, Wand2 } from "lucide-react"
 import { Textarea } from "./components/ui/textarea";
 import { Label } from "./components/ui/label";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
+import { Slider } from "./components/ui/slider";
+
 
 export function App() {  
 
@@ -55,10 +58,77 @@ export function App() {
             </label>
             <input type="file" id="video" accept="video/mp4" className="sr-only" />
             <Separator />
-            <div className="space-y-1">
+            <div className="space-y-2">
               <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
-              <Textarea id="transcription_prompt" className="min-h-20 leading-relaxed"></Textarea>
+              <Textarea 
+                id="transcription_prompt" 
+                className="h-20 leading-relaxed resize-none"
+                placeholder="Inclua palavras-chave mencionadas no video separadas por vírgula (,)"
+              />
             </div>
+            <Button type="submit" className="w-full">
+              Carregar vídeo
+              <Upload className="m-4 h-4 ml-2"/>
+            </Button>
+          </form>
+
+          <Separator/>
+
+          <form className="space-y-6">
+
+          <div className="space-y-2">
+              <Label>Prompt</Label>
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="Selecione um prompt" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="title">Titulo do YouTube</SelectItem>
+                  <SelectItem value="description">Descrição do YouTube</SelectItem>
+                  
+                </SelectContent>
+              </Select>
+            </div>
+
+            <div className="space-y-2">
+              <Label>Modelo</Label>
+              <Select disabled defaultValue="gpt3.5">
+                <SelectTrigger>
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="gpt3.5">GPT 3.5-turbo 16k</SelectItem>
+                </SelectContent>
+              </Select>
+              <span className="block text-xs text-muted-foreground italic">
+                Você poderá customizar essa opção em breve.
+              </span>
+            </div>
+
+            <Separator/>
+
+            <div className="space-y-4">
+              <Label>Temparatura</Label>
+              <Slider 
+                min={0}
+                max={1}
+                step={0.1}
+              />
+
+              <span className="block text-xs text-muted-foreground italic leading-relaxed ">
+                Valores mais altos tendem a deixar mais criativo e com possíveis erros
+              </span>
+            </div>
+
+            <Separator/>
+
+            <Button type="submit" className="w-full">
+              Executar 
+              <Wand2 className="w-4 h-4 ml-2"></Wand2>
+            </Button>
+
+
+
           </form>
 
         </aside>
