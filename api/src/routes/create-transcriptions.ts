c7697c4daf4bbd328d3f2f2773e3c557c8168de4
jackdenio <deniogomes@gmail.com>
2023-10-26 23:12:19 -0300

    Aula 02 - Avançando o back-end da aplicação

--- api/src/routes/create-transcriptions.ts
+++ api/src/routes/create-transcriptions.ts
@@ -3,11 +3,22 @@ import { z } from 'zod';
 import { prisma } from "../lib/prisma";
 
 export async function createTranscriptionRoute(app: FastifyInstance) {
-  app.post('/upload-videos/:videoId/transcription', async(res) => {
+  app.post('/upload-videos/:videoId/transcription', async(req) => {
     const paramsSchema = z.object({
-      videoId = z.string().uuid(),
+      videoId : z.string().uuid(),
     })
-    const videoId = paramsSchema.parse(req.params)
+    const { videoId } = paramsSchema.parse(req.params)
+
+     const bodySchema = z.object({
+      prompt: z.string(),
+     })
+
+     const { prompt } = bodySchema.parse(req.body)
+
+     return {
+      videoId, 
+      prompt,
+     } 
   })
 
   
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,13 @@
 import { fastify } from 'fastify'
 import { getAllPromptRoute } from "./routes/get-all-prompts"
 import { uploadVideosRoute } from "./routes/upload-videos"
+import { createTranscriptionRoute } from "./routes/create-transcriptions"
 
 const app = fastify()
 
 app.register(getAllPromptRoute)
 app.register(uploadVideosRoute)
+app.register(createTranscriptionRoute)
 
 app.listen({
   port: 3333,
